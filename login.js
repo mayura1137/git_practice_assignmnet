f5a4e0676ed405a00cd45bdc6c02dfaf37531864
unknown <mvjadhav1997@gma.com>
2022-11-03 22:32:29 +0530

    Fix this prime code

---
+++ login.js
@@ -0,0 +1,18 @@
+unction checkPrime(num) {
+  let factor = 0
+  for (let i = 0; i < num.length; i++) {
+    if (num % i == 0) {
+      factor++
+    }
+  } if (factor == 2) {
+    return true
+  }
+  return false
+}
+let ans = checkPrime(15)
+
+if (ans == true) {
+  console.log("Prime")
+} else {
+  console.log("Not prime")
+}
